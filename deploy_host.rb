054b2acc46f623b79449b8eb45a4766563085f65
Thomas Powell <twilliampowell@gmail.com>
2022-05-26 23:18:31 -0500

    Lets Encrypt and such

--- deploy_host.rb
+++ deploy_host.rb
@@ -2,32 +2,42 @@
 
 require 'erb'
 require 'etc'
+require 'securerandom'
+require 'net/http'
 
 # based originally on my
 # https://thomaspowell.com/2017/07/03/migrating-wordpress-dreamhost-linode/
 # post
 
+def reload_apache
+  `service apache2 reload`
+end
+
 DOMAIN=ARGV[0]
 USER=ARGV[1]
 etc_passwd=Etc.getpwnam(USER)
 
+`sudo apt-get install -y apache2 mysql-client mysql-server`
 
-conf_template=<<-TEMPLATE
+http_template=<<-HTTP_TEMPLATE
 <Directory /var/www/html/<%= DOMAIN %>/public_html>
-  Require all granted
+        Require all granted
 </Directory>
-
 <VirtualHost *:80>
-  ServerName <%= DOMAIN %>
-  ServerAlias www.<%= DOMAIN %>
-  ServerAdmin email@<%= DOMAIN %>
-  DocumentRoot /var/www/html/<%= DOMAIN %>/public_html
-  ErrorLog /var/www/html/<%= DOMAIN %>/logs/error.log
-CustomLog /var/www/html/<%= DOMAIN %>/logs/access.log combined
+        ServerName <%= DOMAIN %>
+        ServerAlias www.<%= DOMAIN %>
+        ServerAdmin webmaster@<%= DOMAIN %>
+        DocumentRoot /var/www/html/<%= DOMAIN %>/public_html
+
+        ErrorLog /var/www/html/<%= DOMAIN %>/logs/error.log
+        CustomLog /var/www/html/<%= DOMAIN %>/logs/access.log combined
 </VirtualHost>
-TEMPLATE
+HTTP_TEMPLATE
 
 https_template=<<-HTTPS_TEMPLATE
+<Directory /var/www/html/<%= DOMAIN %>/public_html>
+        Require all granted
+</Directory>
 <VirtualHost *:80>
         ServerName <%= DOMAIN %>
         ServerAlias www.<%= DOMAIN %>
@@ -56,10 +66,13 @@ HTTPS_TEMPLATE
 
 
 # Set up the virtual host
-conf_file=ERB.new(conf_template)
-conf_filename=File.join('/var/www/html', "#{DOMAIN}.conf")
+conf_file=ERB.new(http_template)
+conf_filename=File.join('/etc/apache2/sites-available', "#{DOMAIN}.conf")
 File.write(conf_filename, conf_file.result)
 
+`a2ensite #{DOMAIN}`
+
+reload_apache
 
 def remodown(perms, etc_passwd, filepath)
   File.chmod(perms, filepath)
@@ -73,17 +86,28 @@ remodown(0644, etc_passwd, conf_filename)
   File.join('/var/www/html/', DOMAIN),
   File.join('/var/www/html/', DOMAIN, 'logs')
 ].each do |dir_path|
-  Dir.mkdir(dir_path)
+  Dir.mkdir(dir_path) unless Dir.exists?(dir_path)
   remodown(0755, etc_passwd, dir_path)
 end
 
-# set up a db
 
-# CREATE DATABASE wp_example_com;
-# CREATE USER 'examplecom' IDENTIFIED BY '!@(87P@ss';
-# GRANT ALL PRIVILEGES ON wp_example_com.* TO 'examplecom';
-# USE wp_example_com;
-# SOURCE example.com.sql;
+
+# set up a db
+database_name="wp_#{DOMAIN.gsub(/\./, '_')}"
+username=DOMAIN.gsub(/\./,'')
+password=SecureRandom.base64(20)
+db_string=<<DBSTRING
+CREATE DATABASE #{database_name};
+CREATE USER '#{username}' IDENTIFIED BY '#{password}';
+GRANT ALL PRIVILEGES ON #{database_name}.* TO '#{username}';
+DBSTRING
+
+# need to either pass the password or change to trusted root?
+# pass the password with mysql -pP455w0rd (no space between)
+IO.popen("mysql", mode="w+") do |io|
+  io.puts db_string
+  io.close_write
+end
 
 # change wp-config.php for the DB setup
 # // ** MySQL settings – You can get this info from your web host ** //
@@ -102,4 +126,73 @@ end
 
 # Enable the site
 
-`a2ensite #{DOMAIN}`
+
+`
+  cd /var/www/html/#{DOMAIN}/public_html
+  wget https://wordpress.org/latest.tar.gz
+  # curl https://wordpress.org/latest.tar.gz --output latest.tar.gz
+  tar zxvf latest.tar.gz
+  cd wordpress
+  mv * ..
+  cd ..
+  rm -rf wordpress
+`
+
+config_sample=File.open('wp-config-sample.php').read
+
+def set_secrets(string, key_name, secret)
+  string.gsub(
+    /'#{key_name}',\s*'.*'/,
+    "'#{key_name}', '#{secret}'"
+  )
+end
+
+config = set_secrets(config, 'DB_NAME', database_name)
+config = set_secrets(config, 'DB_USER', username)
+config = set_secrets(config, 'DB_PASSWORD', password)
+
+keys=%w(AUTH_KEY SECURE_AUTH_KEY LOGGED_IN_KEY NONCE_KEY AUTH_SALT SECURE_AUTH_SALT LOGGED_IN_SALT NONCE_SALT)
+config = keys.inject(config) do |config, key|
+  set_secrets(config, key, SecureRandom::hex(16))
+end
+
+File.open('wp-config.php', 'w') do |f|
+  f.puts config
+end
+
+# change wp-config.php for the DB setup
+# // ** MySQL settings – You can get this info from your web host ** //
+# /** The name of the database for WordPress */
+# define('DB_NAME', 'wp_example_com');
+#
+# /** MySQL database username */
+# define('DB_USER', 'examplecom');
+#
+# /** MySQL database password */
+# define('DB_PASSWORD', '!@(87P@ss');
+#
+# /** MySQL hostname */
+# define('DB_HOST', 'localhost');
+#
+
+letsencryptupdate='/usr/local/sbin/letsencryptupdate.sh'
+
+`./letsencrypt-auto certonly -d #{DOMAIN}`
+
+conf_file=ERB.new(https_template)
+conf_filename=File.join('/etc/apache2/sites-available', "#{DOMAIN}.conf")
+File.write(conf_filename, conf_file.result)
+
+reload_apache
+
+
+lines=File.open(letsencryptupdate, 'r').readlines
+lines.insert(-2, "/opt/letsencrypt/letsencrypt-auto certonly --quiet --apache --renew-by-default -d #{DOMAIN} >> /var/log/letsencrypt/letsencrypt-auto-update.log")
+
+FileUtils.copy(letsencryptupdate, "#{letsencryptupdate}.#{$$}")
+
+File.open(letsencryptupdate, 'w') do |f|
+  lines.each { |line| f.puts line }
+end
+
+reload_apache
